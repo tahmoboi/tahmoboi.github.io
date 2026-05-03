609b8c8d3d938b4d296869dc138558f0c753f86f
tahmoboi <tahmid21hossain@gmail.com>
2026-05-03 03:03:00 -0500

    quarto blog push

---
+++ rscript.R
@@ -0,0 +1,14 @@
+
+remotes::install_github("smach/newpost", build_vignettes = TRUE)
+
+library(newpost)
+
+newpost::newpost('data story 1')
+
+newpost::newpost('data story 2')
+
+newpost::newpost('data story 3')
+
+newpost::newpost('data story 4')
+
+newpost::newpost('data story 5')
